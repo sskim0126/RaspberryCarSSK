5ed25a4432d5ebcd93310ed2afb5a3b40f6f3708
Kim Sungsik <sskim0126@kookmin.ac.kr>
2017-11-08 15:47:30 +0900

    modify Rascar file

--- Rascar.py
+++ Rascar.py
@@ -18,4 +18,51 @@ class setup:
         GPIO.setmode(GPIO.BOARD)
 
     def set_motor_mode(self):
-        pass
+        self.forward0 = True
+        self.forward1 = False
+        self.backward0 = False
+        self.backward1 = True
+        # set left motor pins
+        self.MotorLeft_A = 12
+        self.MotorLeft_B = 11
+        self.MotorLeft_PWM = 35
+        # set right motor pins
+        self.MotorRight_A = 15
+        self.MotorRight_B = 13
+        self.MotorRight_PWM = 37
+
+    def leftmotor(self, x):
+        if x == True:
+            GPIO.output(self.MotorLeft_A, GPIO.HIGH)
+            GPIO.output(self.MotorLeft_B, GPIO.LOW)
+        elif x == False:
+            GPIO.output(self.MotorLeft_A, GPIO.LOW)
+            GPIO.output(self.MotorLeft_B, GPIO.HIGH)
+        else:
+            print('Config Error')
+
+    def rightmotor(self, x):
+        if x == True:
+            GPIO.output(self.MotorRight_A, GPIO.HIGH)
+            GPIO.output(self.MotorRight_B, GPIO.LOW)
+        elif x == False:
+            GPIO.output(self.MotorRight_A, GPIO.LOW)
+            GPIO.output(self.MotorRight_B, GPIO.HIGH)
+        else:
+            print('Config Error')
+
+    def go_forward_any(self, speed):
+        while True:
+            GPIO.output(self.MotorLeft_PWM, GPIO.HIGH)
+            GPIO.output(self.MotorRight_PWM, GPIO.HIGH)
+            self.leftmotor(self.forward0)
+            self.rightmotor(self.forward0)
+
+class goModule:
+    pass
+
+class trackingSensor:
+    pass
+
+class ultarSensor:
+    pass
