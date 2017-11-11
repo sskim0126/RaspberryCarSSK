6d16809e03e18654c7cd9fbcf4684ac0a537a97d
Kim Sungsik <sskim0126@kookmin.ac.kr>
2017-11-11 14:17:48 +0900

    add ultra sensor, 5-way tracking sensor modules

--- TrackingSensor.py
+++ TrackingSensor.py
@@ -3,7 +3,27 @@
 # Purpose : 5-way tracking sensor module for assignment 4
 # To do : 5-way tracking sensor module
 
-import RPi.GPIO
+import RPi.GPIO as GPIO
 
 class trackingSensor:
-    pass
+    def set_tracking_mode(self):
+        self.leftmostled = 16
+        self.leftlessled = 18
+        self.centerled = 22
+        self.rightlessled = 40
+        self.rightmostled = 32
+
+        GPIO.setup(self.leftmostled, GPIO.IN)
+        GPIO.setup(self.leftlessled, GPIO.IN)
+        GPIO.setup(self.centerled, GPIO.IN)
+        GPIO.setup(self.rightlessled, GPIO.IN)
+        GPIO.setup(self.rightmostled, GPIO.IN)
+
+    def get_isLine(self):
+        # 0 : black line
+        # 1 : whilt ground
+        return [str(GPIO.input(self.leftmostled)),
+                str(GPIO.input(self.leftlessled)),
+                str(GPIO.input(self.centerled)),
+                str(GPIO.input(self.rightlessled)),
+                str(GPIO.input(self.rightmostled))]
--- UltraSensor.py
+++ UltraSensor.py
@@ -3,7 +3,28 @@
 # Purpose : ultra sensor module for assignment 4
 # To do : ultra sensor module
 
-import RPi.GPIO
+import RPi.GPIO as GPIO
+import time
 
 class ultraSensor:
-    pass
+    def set_ultra_mode(self):
+        self.trig = 33
+        self.echo = 31
+
+        GPIO.setup(self.trig, GPIO.OUT)
+        GPIO.setup(self.echo, GPIO.IN)
+
+    def get_distance(self):
+        GPIO.output(self.trig, False)
+        time.sleep(0.5)
+        GPIO.output(self.trig, True)
+        time.sleep(0.00001)
+        GPIO.output(self.trig, False)
+        while GPIO.input(self.echo) == 0:
+            pulse_start = time.time()
+        while GPIO.input(self.echo) == 1:
+            pulse_end = time.time()
+        pulse_duration = pulse_end - pulse_start
+        distance = pulse_duration * 17000
+        distance = round(distance, 2)
+        return distance
