5ac00860077bc8f6546db07b82b4c7d31dcd694a
Kim Sungsik <sskim0126@kookmin.ac.kr>
2017-12-04 09:45:33 +0900

    Update main.py

--- 5차과제/main.py
+++ 5차과제/main.py
@@ -1,4 +1,4 @@
-# 2017.11.29
+# 2017.11.06
 # Team member : Kim Sungsik, Kim Sujin, Kim Gyuri
 # Purpose : main code for assignment
 # To do : line tracing, avoid obstacle
@@ -16,37 +16,45 @@ if __name__ == "__main__":
             is_line = get_is_line()
             print(is_line)
             if is_line[4] == 0:
-                stop(0.3)
+                stop()
+                sleep(0.3)
                 go_forward(40, 0.3)
-                stop(0.3)
+                stop()
+                sleep(0.3)
                 rightPointTurn(40, 0.3)
                 while is_line[2] != 0:
                     is_line = get_is_line()
-                    rightPointTurn(40, 0.1)
+                    rightPointTurn(40, 0.01)
             else:
                 if is_line[1] == 0:
                     go_forward(40, 0.3)
                     if is_line == [1, 1, 1, 1, 1]:
-                        stop(0.3)
+                        stop()
+                        sleep(0.3)
                         leftPointTurn(40, 0.3)
                         while is_line[2] != 0:
-                            is_line = get_is_line()
-                            leftPointTurn(40, 0.1)
+                            leftPointTurn(40, 0.01)
                 elif is_line == [1, 1, 1, 1, 1]:
-                    stop(0.3)
+                    stop()
+                    sleep(0.3)
                     go_forward(40, 0.2)
                     while is_line[2] != 0:
                         rightPointTurn(40, 0.01)
                 elif is_line == [1, 0, 1, 1, 1]:
-                    curve_turn(30, 40)
+                    curve_turn(15, 30)
+                    sleep(0.05)
                 elif is_line == [1, 0, 0, 1, 1]:
-                    curve_turn(35, 40)
+                    curve_turn(20, 30)
+                    sleep(0.05)
                 elif is_line == [1, 1, 1, 0, 1]:
-                    curve_turn(40, 30)
+                    curve_turn(30, 15)
+                    sleep(0.05)
                 elif is_line == [1, 1, 0, 0, 1]:
-                    curve_turn(40, 35)
+                    curve_turn(30, 20)
+                    sleep(0.05)
                 else:
                     go_forward_any(40)
+                    sleep(0.05)
 
 
 
