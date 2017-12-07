1d7d16ffefe3949b8ff78247206521a97d233fb0
rosa980309 <rosa980309@kookmin.ac.kr>
2017-12-07 07:58:21 +0000

    modify main in rascar

--- 5차과제/main.py
+++ 5차과제/main.py
@@ -22,38 +22,55 @@ if __name__ == "__main__":
                 stop()
                 sleep(0.5)
                 right_point_turn(40, 0.3)
+                is_line = get_is_line()
                 while is_line[2] != 0:
+                    right_point_turn(40, 0.05)
+                    stop()
+                    sleep(0.1)
                     is_line = get_is_line()
-                    right_point_turn(40, 0.01)
             elif is_line[1] == 0:
-                go_forward(40, 0.5)
+                go_forward(40, 0.3)
                 is_line = get_is_line()
                 # 좌회전 코드
                 if is_line == [1, 1, 1, 1, 1]:
                     stop()
                     sleep(0.5)
                     left_point_turn(40, 0.3)
+                    is_line = get_is_line()
                     while is_line[2] != 0:
+                        left_point_turn(40, 0.05)
+                        stop()
+                        sleep(0.1)
                         is_line = get_is_line()
-                        left_point_turn(40, 0.01)
             # 유턴 코드
             elif is_line == [1, 1, 1, 1, 1]:
                 stop()
                 sleep(0.5)
                 go_forward(40, 0.2)
+                stop()
+                right_point_turn(40, 0.6)
+                is_line = get_is_line()
                 while is_line[2] != 0:
-                    right_point_turn(40, 0.01)
+                    right_point_turn(40, 0.05)
+                    stop()
+                    sleep(0.1)
+                    is_line = get_is_line()
             # line tracing
             elif is_line == [1, 0, 1, 1, 1]:
                 curve_turn(15, 40)
+                sleep(0.05)
             elif is_line == [1, 0, 0, 1, 1]:
                 curve_turn(20, 40)
+                sleep(0.05)
             elif is_line == [1, 1, 1, 0, 1]:
                 curve_turn(40, 15)
+                sleep(0.05)
             elif is_line == [1, 1, 0, 0, 1]:
                 curve_turn(40, 20)
+                sleep(0.05)
             else:
                 go_forward_any(40)
+                sleep(0.05)
 
 
 
