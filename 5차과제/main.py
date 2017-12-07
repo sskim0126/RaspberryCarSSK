3fa755b5bec352afcd35f0a7ac5f3fec50f339ca
Kim Sungsik <sskim0126@kookmin.ac.kr>
2017-12-07 16:48:05 +0900

    modify main in laptop by kss

--- 5차과제/main.py
+++ 5차과제/main.py
@@ -17,44 +17,43 @@ if __name__ == "__main__":
             # 우회전 코드
             if is_line[4] == 0:
                 stop()
-                sleep(0.3)
+                sleep(0.5)
                 go_forward(40, 0.5)
                 stop()
-                sleep(0.3)
+                sleep(0.5)
                 right_point_turn(40, 0.3)
                 while is_line[2] != 0:
                     is_line = get_is_line()
                     right_point_turn(40, 0.01)
-            else:
-                if is_line[1] == 0:
-                    go_forward(40, 0.5)
-                    is_line = get_is_line()
-                    # 좌회전 코드
-                    if is_line == [1, 1, 1, 1, 1]:
-                        stop()
-                        sleep(0.3)
-                        left_point_turn(40, 0.3)
-                        while is_line[2] != 0:
-                            is_line = get_is_line()
-                            left_point_turn(40, 0.01)
-                # 유턴 코드
-                elif is_line == [1, 1, 1, 1, 1]:
+            elif is_line[1] == 0:
+                go_forward(40, 0.5)
+                is_line = get_is_line()
+                # 좌회전 코드
+                if is_line == [1, 1, 1, 1, 1]:
                     stop()
-                    sleep(0.3)
-                    go_forward(40, 0.2)
+                    sleep(0.5)
+                    left_point_turn(40, 0.3)
                     while is_line[2] != 0:
-                        right_point_turn(40, 0.01)
-                # line tracing
-                if is_line == [1, 0, 1, 1, 1]:
-                    curve_turn(15, 40)
-                elif is_line == [1, 0, 0, 1, 1]:
-                    curve_turn(20, 40)
-                elif is_line == [1, 1, 1, 0, 1]:
-                    curve_turn(40, 15)
-                elif is_line == [1, 1, 0, 0, 1]:
-                    curve_turn(40, 20)
-                else:
-                    go_forward_any(40)
+                        is_line = get_is_line()
+                        left_point_turn(40, 0.01)
+            # 유턴 코드
+            elif is_line == [1, 1, 1, 1, 1]:
+                stop()
+                sleep(0.5)
+                go_forward(40, 0.2)
+                while is_line[2] != 0:
+                    right_point_turn(40, 0.01)
+            # line tracing
+            elif is_line == [1, 0, 1, 1, 1]:
+                curve_turn(15, 40)
+            elif is_line == [1, 0, 0, 1, 1]:
+                curve_turn(20, 40)
+            elif is_line == [1, 1, 1, 0, 1]:
+                curve_turn(40, 15)
+            elif is_line == [1, 1, 0, 0, 1]:
+                curve_turn(40, 20)
+            else:
+                go_forward_any(40)
 
 
 
