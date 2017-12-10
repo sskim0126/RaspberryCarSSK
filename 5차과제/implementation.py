0255318e4117bc5e6206d579f72e48b5be3f3fae
Sujin-Kim1 <agatha197@kookmin.ac.kr>
2017-12-10 23:31:10 +0900

    implementation code

---
+++ 5차과제/implementation.py
@@ -0,0 +1,68 @@
+# 2017.11.06
+# Team member : Kim Sungsik, Kim Sujin, Kim Gyuri
+# Purpose : main code for assignment
+# To do : line tracing, avoid obstacle
+
+import RPi.GPIO as GPIO
+from TrackingSensor import *
+from Rascar import *
+
+if __name__ == "__main__":
+    pwm_setup()
+    GPIO.setwarnings(False)
+    try:
+        while True:
+            is_line = get_is_line()
+            print(is_line)
+            # 우회전 코드
+            if is_line[4] == 0:
+                stop()
+                sleep(0.5)
+                go_forward(40, 0.5)
+                stop()
+                sleep(0.5)
+                right_point_turn(40, 0.3)
+                is_line = get_is_line()
+                while is_line[2] != 0:
+                    right_point_turn(40, 0.05)
+                    stop()
+                    sleep(0.1)
+                    is_line = get_is_line()
+
+            # line tracing
+            elif is_line == [1, 0, 1, 1, 1]:
+                curve_turn(15, 40)
+                sleep(0.05)
+            elif is_line == [1, 0, 0, 1, 1]:
+                curve_turn(20, 40)
+                sleep(0.05)
+            elif is_line == [1, 1, 1, 0, 1]:
+                curve_turn(40, 15)
+                sleep(0.05)
+            elif is_line == [1, 1, 0, 0, 1]:
+                curve_turn(40, 20)
+                sleep(0.05)
+            elif is_line == [1, 1, 0, 1, 1]:
+                go_forward_any(40)
+                sleep(0.05)
+
+            # 유턴 코드
+            elif is_line == [1, 1, 1, 1, 1]:
+                stop()
+                sleep(0.5)
+                go_forward(40, 0.2)
+                stop()
+                right_point_turn(40, 0.6)
+                is_line = get_is_line()
+                while is_line[2] != 0:
+                    right_point_turn(40, 0.05)
+                    stop()
+                    sleep(0.1)
+                    is_line = get_is_line()
+
+
+
+
+
+    except KeyboardInterrupt:
+        pwm_low()
